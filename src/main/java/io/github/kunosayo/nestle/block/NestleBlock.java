897224cb7a1884a162f6eccac878a97705fae7e9
KunoSayo <KunoSayo@outlook.com>
2024-09-05 12:55:38 +0800

    textures

--- src/main/java/io/github/kunosayo/nestle/block/NestleBlock.java
+++ src/main/java/io/github/kunosayo/nestle/block/NestleBlock.java
@@ -11,6 +11,7 @@ import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.HorizontalDirectionalBlock;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
@@ -21,13 +22,13 @@ import javax.annotation.Nullable;
 import javax.annotation.ParametersAreNonnullByDefault;
 
 @ParametersAreNonnullByDefault
-public class NestleBlock extends Block {
-    public static BooleanProperty INVERTED = BlockStateProperties.INVERTED;
+public class NestleBlock extends HorizontalDirectionalBlock {
+    public static BooleanProperty POWERED = BlockStateProperties.POWERED;
     public static final MapCodec<NestleBlock> CODEC = simpleCodec(NestleBlock::new);
 
     public NestleBlock(BlockBehaviour.Properties properties) {
         super(properties);
-        this.registerDefaultState(this.defaultBlockState().setValue(INVERTED, Boolean.FALSE));
+        this.registerDefaultState(this.defaultBlockState().setValue(POWERED, Boolean.FALSE));
     }
 
     @Override
@@ -41,7 +42,7 @@ public class NestleBlock extends Block {
             return;
         }
         if (entity instanceof LivingEntity livingEntity) {
-            if (state.getValue(INVERTED)) {
+            if (state.getValue(POWERED)) {
                 livingEntity.addEffect(new MobEffectInstance(ModEffects.DESIRE_NESTLE_EFFECT, 100));
             } else {
                 livingEntity.addEffect(new MobEffectInstance(ModEffects.NESTLE_EFFECT, 100));
@@ -53,18 +54,20 @@ public class NestleBlock extends Block {
     @Nullable
     @Override
     public BlockState getStateForPlacement(BlockPlaceContext context) {
-        return this.defaultBlockState().setValue(INVERTED, Boolean.valueOf(context.getLevel().hasNeighborSignal(context.getClickedPos())));
+        return this.defaultBlockState()
+                .setValue(FACING, context.getHorizontalDirection())
+                .setValue(POWERED, context.getLevel().hasNeighborSignal(context.getClickedPos()));
     }
 
     @Override
     protected void neighborChanged(BlockState state, Level level, BlockPos pos, Block block, BlockPos fromPos, boolean isMoving) {
         if (!level.isClientSide) {
-            boolean flag = state.getValue(INVERTED);
+            boolean flag = state.getValue(POWERED);
             if (flag != level.hasNeighborSignal(pos)) {
                 if (flag) {
                     level.scheduleTick(pos, this, 4);
                 } else {
-                    level.setBlock(pos, state.cycle(INVERTED), UPDATE_CLIENTS);
+                    level.setBlock(pos, state.cycle(POWERED), UPDATE_CLIENTS);
                 }
             }
         }
@@ -72,13 +75,15 @@ public class NestleBlock extends Block {
 
     @Override
     protected void tick(BlockState state, ServerLevel level, BlockPos pos, RandomSource random) {
-        if (state.getValue(INVERTED) && !level.hasNeighborSignal(pos)) {
-            level.setBlock(pos, state.cycle(INVERTED), UPDATE_CLIENTS);
+        if (state.getValue(POWERED) && !level.hasNeighborSignal(pos)) {
+            level.setBlock(pos, state.cycle(POWERED), UPDATE_CLIENTS);
         }
     }
 
     @Override
     protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
-        builder.add(INVERTED);
+        builder.add(POWERED);
+        builder.add(FACING);
     }
+
 }
--- src/main/java/io/github/kunosayo/nestle/datagen/NestleBlocksModelProvider.java
+++ src/main/java/io/github/kunosayo/nestle/datagen/NestleBlocksModelProvider.java
@@ -1,13 +1,11 @@
 package io.github.kunosayo.nestle.datagen;
 
 import io.github.kunosayo.nestle.Nestle;
+import io.github.kunosayo.nestle.block.NestleBlock;
 import io.github.kunosayo.nestle.init.ModBlocks;
-import net.minecraft.core.Direction;
 import net.minecraft.data.PackOutput;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.level.block.GlazedTerracottaBlock;
 import net.neoforged.neoforge.client.model.generators.BlockStateProvider;
-import net.neoforged.neoforge.client.model.generators.ModelFile;
 import net.neoforged.neoforge.common.data.ExistingFileHelper;
 
 public class NestleBlocksModelProvider extends BlockStateProvider {
@@ -50,9 +48,76 @@ public class NestleBlocksModelProvider extends BlockStateProvider {
         horizontalBlock(ModBlocks.NESTLE_RESISTANCE_BLOCK.get(), model);
     }
 
+    private void registerNestleBlock() {
+        ResourceLocation bottomTexture = modLoc("block/nestle_block_bottom");
+        ResourceLocation topTexture = modLoc("block/nestle_block_top");
+        ResourceLocation sideTexture = modLoc("block/nestle_block_side");
+        ResourceLocation frontTexture = modLoc("block/nestle_resistance_block_front");
+        ResourceLocation backTexture = modLoc("block/nestle_block_back");
+
+        ResourceLocation bottomPoweredTexture = modLoc("block/nestle_block_bottom_powered");
+        ResourceLocation topPoweredTexture = modLoc("block/nestle_block_top_powered");
+        ResourceLocation sidePoweredTexture = modLoc("block/nestle_block_side_powered");
+        ResourceLocation frontPoweredTexture = modLoc("block/nestle_resistance_block_front");
+        ResourceLocation backPoweredTexture = modLoc("block/nestle_block_back");
+
+
+        var model = models().withExistingParent("nestle_block", "minecraft:block/cube")
+                .texture("top", topTexture)
+                .texture("bottom", bottomTexture)
+                .texture("side", sideTexture)
+                .texture("front", frontTexture)
+                .texture("back", backTexture)
+                .element()
+                .from(0.0f, 0.0f, 0.0f)
+                .to(16.0f, 16.0f, 16.0f)
+                .allFaces((direction, faceBuilder) -> {
+                    faceBuilder.cullface(direction);
+                    switch (direction) {
+                        case DOWN -> faceBuilder.texture("#bottom");
+                        case UP -> faceBuilder.texture("#top");
+                        case NORTH -> faceBuilder.texture("#back");
+                        case SOUTH -> faceBuilder.texture("#front");
+                        case WEST, EAST -> faceBuilder.texture("#side");
+                    }
+                    switch (direction) {
+                        case DOWN, UP, NORTH, SOUTH, WEST -> faceBuilder.uvs(0.0f, 0.0f, 16.0f, 16.0f);
+                        case EAST -> faceBuilder.uvs(16.0f, 0.0f, 0.0f, 16.0f);
+                    }
+                })
+                .end();
+
+        var poweredModel = models().withExistingParent("nestle_block_powered", "minecraft:block/cube")
+                .texture("top", topPoweredTexture)
+                .texture("bottom", bottomPoweredTexture)
+                .texture("side", sidePoweredTexture)
+                .texture("front", frontPoweredTexture)
+                .texture("back", backPoweredTexture)
+                .element()
+                .from(0.0f, 0.0f, 0.0f)
+                .to(16.0f, 16.0f, 16.0f)
+                .allFaces((direction, faceBuilder) -> {
+                    faceBuilder.cullface(direction);
+                    switch (direction) {
+                        case DOWN -> faceBuilder.texture("#bottom");
+                        case UP -> faceBuilder.texture("#top");
+                        case NORTH -> faceBuilder.texture("#back");
+                        case SOUTH -> faceBuilder.texture("#front");
+                        case WEST, EAST -> faceBuilder.texture("#side");
+                    }
+                    switch (direction) {
+                        case DOWN, UP, NORTH, SOUTH, WEST -> faceBuilder.uvs(0.0f, 0.0f, 16.0f, 16.0f);
+                        case EAST -> faceBuilder.uvs(16.0f, 0.0f, 0.0f, 16.0f);
+                    }
+                })
+                .end();
+
+        horizontalBlock(ModBlocks.NESTLE_BLOCK.get(), state -> state.getValue(NestleBlock.POWERED) ? poweredModel : model);
+    }
+
     @Override
     protected void registerStatesAndModels() {
-
         registerNestleResistanceBlock();
+        registerNestleBlock();
     }
 }
